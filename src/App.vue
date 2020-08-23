5c4b41c180300cb811a2e20da3ba4725d271a127
AnastasiiaChery <zamorena.nastya@gmail.com>
2020-08-23 16:59:34 +0300

    vue1

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input type="text" v-model="nevPl">
+    <button @click="save" v-if="plans.length <= 10">Save</button>
+    <ul>
+      <li v-for="(item, i) in plans" :key="i">
+        <p>{{item}}</p>
+        <button @click="del(i)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWork',
+  data() {
+    return {
+      nevPl: '',
+      plans: []
+    }
+  },
+  methods: {
+    save() {
+      this.plans.push(this.nevPl);
+      this.nevPl = '';
+    },
+    del(i) {
+      this.plans.splice(i, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
